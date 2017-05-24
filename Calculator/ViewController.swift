fa01a1b320404f02329f951ab3c61f2158ba6b7a
Zi Yi Hua <sherryhua2012@hotmail.com>
2017-05-24 01:00:37 -0400

    Add all existing project files

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sherilyn Hua on 2017-05-23.
+//  Copyright © 2017 Sherilyn Hua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userInMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userInMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userInMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userInMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
+}
+
