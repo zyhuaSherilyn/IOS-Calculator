4ccb75fa934d038c3b3d5affd9722b23630d62a8
Zi Yi Hua <sherryhua2012@hotmail.com>
2017-05-25 01:29:06 -0400

    Add extensible operations in the form of an enum

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,22 +8,38 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
 struct CalculatorBrain {
     
     private var accumulator: Double? //no accumulated result in the beginning
     
+    private enum Operation { // a mixed type for the operations dictionary
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation(sqrt),
+        "cos" : Operation.unaryOperation(cos),
+        "±" : Operation.unaryOperation(changeSign)
+    ]
+    
     mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
             }
-        default:
-            break
         }
-        
     }
     
     // structs copy on write
